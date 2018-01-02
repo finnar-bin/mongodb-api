06816e91abe097e8776201e3662015ed8ec358df
Nar Cuenca <lunar.cuenca@nchcss.com>
2018-01-02 13:30:37 +0800

    add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
-})
+});
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,19 @@ const request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+//seeder
+const seederTodos = [{
+    text : 'First test'
+}, {
+    text : 'Second test'
+}];
+
 //used to make preparations before test is run
 //remove all entries in db before running test below
 beforeEach((done) => {
     Todo.remove({}).then(() => {
+        Todo.insertMany(seederTodos);
+    }).then(() => {
         done();
     });
 });
@@ -28,7 +37,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find({}).then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -51,11 +60,23 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find({}).then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((err) => {
                     done(err);
                 })
             });
     })
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done());
+    });
 });
