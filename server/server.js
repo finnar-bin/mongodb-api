163f4d77a078a5ee505a8e40a60626076d589d94
Nar Cuenca <lunar.cuenca@nchcss.com>
2018-01-03 13:29:06 +0800

    added Heroku config

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 //middleware return value = json
 app.use(bodyParser.json());
@@ -53,8 +54,8 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000...');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}...`);
 });
 
 module.exports = {
