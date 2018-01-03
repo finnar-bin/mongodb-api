e9f33141f356053751f0f8729e5cce2ec8f0523c
Nar Cuenca <lunar.cuenca@nchcss.com>
2018-01-03 17:12:20 +0800

    added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
-var {mongoose} = require('./db/mongoose'); //establish db connection
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose'); //establish db connection
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
-var app = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 //middleware return value = json
@@ -70,6 +71,32 @@ app.delete('/todos/:id', (req, res) => {
 
         res.status(200).send({todo});
     }).catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set : body}, {new : true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(port, () => {
