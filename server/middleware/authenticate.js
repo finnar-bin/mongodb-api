a00bcbcd2f973f6d809a70caee3d21fdef7d59eb
Nar Cuenca <lunar.cuenca@nchcss.com>
2018-01-08 16:31:55 +0800

    added GET /users/me and authenticate middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,22 @@
+var {User} = require('./../models/user');
+
+// middleware to make routes private
+var authenticate = (req, res, next) => {
+    var token = req.header('x-auth');
+    
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch((e) => {
+        res.status(401).send();
+    });
+};
+
+module.exports = {
+    authenticate
+};
