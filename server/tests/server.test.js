ffd551ac5d9164a40f3e2dbc3aaf346c413d6481
Nar Cuenca <lunar.cuenca@nchcss.com>
2018-01-09 17:13:27 +0800

    added POST /users/login test scripts

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -231,7 +231,7 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
-                });
+                }).catch((e) => done(e));
             });
     });
 
@@ -264,3 +264,55 @@ describe('POST /users', () => {
             .end(done);
     });
 });
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email : seederUsers[1].email,
+                password : seederUsers[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(seederUsers[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access : 'auth',
+                        token : res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e));      
+            });
+    });
+
+    it('should reject invalid login', (done) => {
+        // invalid password - 400 - xauth not exist - user tokens = 0
+        request(app)
+            .post('/users/login')
+            .send({
+                email : seederUsers[1].email,
+                password : seederUsers[1].password + '123'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(seederUsers[1]._id).then((user) => {
+                    expect(user.tokens[0]).toBe(undefined);
+                    done();
+                }).catch((e) => done(e)); 
+            });
+    });
+})
