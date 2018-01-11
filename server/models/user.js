a1a477ec31a9442e1917119bb25450aa7b49b00d
Nar Cuenca <lunar.cuenca@nchcss.com>
2018-01-11 13:34:29 +0800

    updated config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -49,7 +49,7 @@ UserSchema.methods.generateAuthToken = function () {
     var token = jwt.sign({
         _id : user._id.toHexString(),
         access
-    }, 'abc123').toString();
+    }, process.env.JWT_SECRET).toString();
 
     //push new variables to tokens array
     user.tokens = user.tokens.concat([{access, token}]);
@@ -75,7 +75,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         return Promise.reject();
     };
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -16,7 +16,7 @@ const seederUsers = [{
         token : jwt.sign({
             _id : userOneId,
             access : 'auth'
-        }, 'abc123').toString()
+        }, process.env.JWT_SECRET).toString()
     }]
 }, {
     _id : userTwoId,
@@ -27,7 +27,7 @@ const seederUsers = [{
         token : jwt.sign({
             _id : userTwoId,
             access : 'auth'
-        }, 'abc123').toString()
+        }, process.env.JWT_SECRET).toString()
     }]
 }];
 
